4e75b9ae7eb1d862123d360c5ae92462ab10d504
LouisXZ <louiszhouxh@gmail.com>
2017-10-23 22:52:00 -0700

    End-to-End Test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,38 @@
-import { AppPage } from './app.po';
+import { ConFusionPage } from './app.po';
+import { browser } from 'protractor';
 
 describe('con-fusion App', () => {
-  let page: AppPage;
+  let page: ConFusionPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new ConFusionPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  it('should display Ristorante Con Fusion', () => {
+    page.navigateTo('/');
+    expect(page.getParagraphText('app-root h1')).toEqual('Ristorante Con Fusion');
+  });
+
+  it('shoud navigate to about us page by clicking on the link', () => {
+    page.navigateTo('/');
+
+    const navLink = page.getAllElements('a').get(1);
+    navLink.click();
+
+    expect(page.getParagraphText('h3')).toBe('About Us');
+  });
+
+  it('should enter a new comment for the first dish', () => {
+    page.navigateTo('/dishdetail/0');
+    const newAuthor = page.getElement('input[type=text]');
+    newAuthor.sendKeys('Test Author');
+
+    const newComment = page.getElement('textarea');
+    newComment.sendKeys('Test Comment');
+
+    const newSubmitButton = page.getElement('button[type=submit]');
+    newSubmitButton.click();
+
+    browser.sleep(10000);
   });
 });
